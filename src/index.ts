87274c09e4b576665142a6a3351899f2f367f1dd
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-10-09 14:24:18 +0900

    🎨 Functionalize main processing

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import createDebug from 'debug';
 import Metalsmith from 'metalsmith';
 import path from 'path';
 
-import { InputOptions, normalizeOptions } from './options';
+import { InputOptions, normalizeOptions, OptionsInterface } from './options';
 import { hasProp } from './utils';
 import {
     addFile,
     createPlugin,
+    FileInterface,
     getMatchedFilenameList,
     getValidFiles,
 } from './utils/metalsmith';
@@ -15,6 +16,87 @@ import { findSourceMapFile, getSourceMappingURL } from './utils/source-map';
 
 const debug = createDebug(require('../package.json').name);
 const debugPostcssrc = debug.extend('postcssrc');
+
+async function processFile({
+    files,
+    metalsmith,
+    options,
+    filename,
+    filedata,
+}: {
+    files: Metalsmith.Files;
+    metalsmith: Metalsmith;
+    options: OptionsInterface;
+    filename: string;
+    filedata: FileInterface;
+}): Promise<void> {
+    const newFilename = options.renamer(filename);
+
+    const from = metalsmith.path(metalsmith.source(), filename);
+    const to = metalsmith.path(metalsmith.destination(), newFilename);
+    const config = await loadConfig({
+        options: { ...options.options, from, to },
+        sourceFilepath: from,
+        metalsmith,
+    });
+    if (config) {
+        debugPostcssrc(
+            'loaded postcss config by file %o: %o',
+            filename,
+            path.relative(process.cwd(), config.file),
+        );
+    }
+    const plugins = config ? config.plugins : [...options.plugins];
+    const postcssOptions = {
+        ...(config ? config.options : options.options),
+        from,
+        to,
+    };
+
+    const postcssMapOption = postcssOptions.map;
+    if (
+        postcssMapOption &&
+        !(
+            typeof postcssMapOption === 'object' &&
+            hasProp(postcssMapOption, 'prev')
+        )
+    ) {
+        const [, sourceMapFiledata] = findSourceMapFile(
+            files,
+            filename,
+            metalsmith,
+        );
+        if (sourceMapFiledata) {
+            const prev = sourceMapFiledata.contents.toString();
+            postcssOptions.map =
+                postcssMapOption === true
+                    ? { prev }
+                    : { prev, ...postcssMapOption };
+        }
+    }
+
+    const result = await processCSS(plugins, filedata.contents, postcssOptions);
+    if (!result) return;
+
+    const cssText = result.css;
+    addFile(files, newFilename, cssText, filedata);
+    if (filename !== newFilename) {
+        debug('done process %o, renamed to %o', filename, newFilename);
+        delete files[filename];
+        debug('file deleted: %o', filename);
+    } else {
+        debug('done process %o', filename);
+    }
+
+    if (result.map) {
+        const sourceMappingURL = getSourceMappingURL(cssText);
+        const sourceMapFilename = sourceMappingURL
+            ? path.join(path.dirname(newFilename), sourceMappingURL)
+            : newFilename + '.map';
+        addFile(files, sourceMapFilename, result.map.toString());
+        debug('generate SourceMap: %o', sourceMapFilename);
+    }
+}
 
 export = (opts: InputOptions = {}): Metalsmith.Plugin => {
     return createPlugin(async (files, metalsmith) => {
@@ -33,85 +115,9 @@ export = (opts: InputOptions = {}): Metalsmith.Plugin => {
         );
 
         await Promise.all(
-            Object.entries(targetFiles).map(async ([filename, filedata]) => {
-                const newFilename = options.renamer(filename);
-
-                const from = metalsmith.path(metalsmith.source(), filename);
-                const to = metalsmith.path(
-                    metalsmith.destination(),
-                    newFilename,
-                );
-                const config = await loadConfig({
-                    options: { ...options.options, from, to },
-                    sourceFilepath: from,
-                    metalsmith,
-                });
-                if (config) {
-                    debugPostcssrc(
-                        'loaded postcss config by file %o: %o',
-                        filename,
-                        path.relative(process.cwd(), config.file),
-                    );
-                }
-                const plugins = config ? config.plugins : [...options.plugins];
-                const postcssOptions = {
-                    ...(config ? config.options : options.options),
-                    from,
-                    to,
-                };
-
-                const postcssMapOption = postcssOptions.map;
-                if (
-                    postcssMapOption &&
-                    !(
-                        typeof postcssMapOption === 'object' &&
-                        hasProp(postcssMapOption, 'prev')
-                    )
-                ) {
-                    const [, sourceMapFiledata] = findSourceMapFile(
-                        files,
-                        filename,
-                        metalsmith,
-                    );
-                    if (sourceMapFiledata) {
-                        const prev = sourceMapFiledata.contents.toString();
-                        postcssOptions.map =
-                            postcssMapOption === true
-                                ? { prev }
-                                : { prev, ...postcssMapOption };
-                    }
-                }
-
-                const result = await processCSS(
-                    plugins,
-                    filedata.contents,
-                    postcssOptions,
-                );
-                if (!result) return;
-
-                const cssText = result.css;
-                addFile(files, newFilename, cssText, filedata);
-                if (filename !== newFilename) {
-                    debug(
-                        'done process %o, renamed to %o',
-                        filename,
-                        newFilename,
-                    );
-                    delete files[filename];
-                    debug('file deleted: %o', filename);
-                } else {
-                    debug('done process %o', filename);
-                }
-
-                if (result.map) {
-                    const sourceMappingURL = getSourceMappingURL(cssText);
-                    const sourceMapFilename = sourceMappingURL
-                        ? path.join(path.dirname(newFilename), sourceMappingURL)
-                        : newFilename + '.map';
-                    addFile(files, sourceMapFilename, result.map.toString());
-                    debug('generate SourceMap: %o', sourceMapFilename);
-                }
-            }),
+            Object.entries(targetFiles).map(async ([filename, filedata]) =>
+                processFile({ files, metalsmith, options, filename, filedata }),
+            ),
         );
     });
 };
