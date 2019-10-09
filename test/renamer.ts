9cebf100d31bcdf540a779075990a976ff37dcbe
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-10-09 20:32:22 +0900

    ✅ Add test for "renamer" option: value of undefined in type definition

--- test/renamer.ts
+++ test/renamer.ts
@@ -1,6 +1,7 @@
 import test from 'ava';
 import Metalsmith from 'metalsmith';
 import path from 'path';
+import util from 'util';
 
 import postcss from '../src/index';
 import { hasProp } from '../src/utils';
@@ -48,88 +49,100 @@ test('should rename sugarss files: not set renamer option', async t => {
     );
 });
 
-for (const renamer of [true, undefined]) {
-    test(`should rename sugarss files: set renamer option to ${renamer}`, async t => {
-        const metalsmith = Metalsmith(fixtures('sugarss'))
-            .source('src')
-            .use(
-                postcss({
-                    pattern: '**/*.sss',
-                    renamer,
-                }),
+for (const renamer of [true, undefined, 42, Infinity, 'str', {}, []]) {
+    test(
+        'should rename sugarss files: set renamer option to ' +
+            util.inspect(renamer),
+        async t => {
+            const metalsmith = Metalsmith(fixtures('sugarss'))
+                .source('src')
+                .use(
+                    postcss({
+                        pattern: '**/*.sss',
+                        renamer:
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                            renamer as any,
+                    }),
+                );
+            const files = await processAsync(metalsmith);
+
+            switchTest(
+                hasProp(files, 'a.css'),
+                'should generate CSS file',
+                msg => {
+                    t.pass(msg);
+                },
+                msg => {
+                    t.fail(msg);
+                    t.log(files);
+                },
+            )(
+                !hasProp(files, 'a.sss'),
+                'should remove original SugarSS file',
+                msg => {
+                    t.pass(msg);
+                },
+                msg => {
+                    t.fail(msg);
+                    t.log(files);
+                },
             );
-        const files = await processAsync(metalsmith);
 
-        switchTest(
-            hasProp(files, 'a.css'),
-            'should generate CSS file',
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files);
-            },
-        )(
-            !hasProp(files, 'a.sss'),
-            'should remove original SugarSS file',
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files);
-            },
-        );
-
-        t.regex(
-            files['a.css'].contents.toString('utf8'),
-            /a\s*\{\s*color:\s*azure\s*\}\s*/,
-            'should convert SugarSS to CSS',
-        );
-    });
+            t.regex(
+                files['a.css'].contents.toString('utf8'),
+                /a\s*\{\s*color:\s*azure\s*\}\s*/,
+                'should convert SugarSS to CSS',
+            );
+        },
+    );
 }
 
-for (const renamer of [false, null]) {
-    test(`should not rename sugarss files: set renamer option to ${renamer}`, async t => {
-        const metalsmith = Metalsmith(fixtures('sugarss'))
-            .source('src')
-            .use(
-                postcss({
-                    pattern: '**/*.sss',
-                    renamer,
-                }),
+for (const renamer of [false, null, 0, NaN, '']) {
+    test(
+        'should not rename sugarss files: set renamer option to ' +
+            util.inspect(renamer),
+        async t => {
+            const metalsmith = Metalsmith(fixtures('sugarss'))
+                .source('src')
+                .use(
+                    postcss({
+                        pattern: '**/*.sss',
+                        renamer:
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                            renamer as any,
+                    }),
+                );
+            const files = await processAsync(metalsmith);
+
+            switchTest(
+                !hasProp(files, 'a.css'),
+                'should not generate .css file',
+                msg => {
+                    t.pass(msg);
+                },
+                msg => {
+                    t.fail(msg);
+                    t.log(files);
+                },
+            )(
+                hasProp(files, 'a.sss'),
+                'should not remove original SugarSS file',
+                msg => {
+                    t.pass(msg);
+                },
+                msg => {
+                    t.fail(msg);
+                    t.log(files);
+                },
             );
-        const files = await processAsync(metalsmith);
 
-        switchTest(
-            !hasProp(files, 'a.css'),
-            'should not generate .css file',
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files);
-            },
-        )(
-            hasProp(files, 'a.sss'),
-            'should not remove original SugarSS file',
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files);
-            },
-        );
-
-        t.regex(
-            files['a.sss'].contents.toString('utf8'),
-            /a\s*\{\s*color:\s*azure\s*\}\s*/,
-            'should convert SugarSS to CSS',
-        );
-    });
+            t.regex(
+                files['a.sss'].contents.toString('utf8'),
+                /a\s*\{\s*color:\s*azure\s*\}\s*/,
+                'should convert SugarSS to CSS',
+            );
+        },
+    );
 }
 
 test('should rename sugarss files with custom renamer', async t => {
