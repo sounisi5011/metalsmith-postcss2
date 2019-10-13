a34fef40d4f3b4970b67255b2bb625b566d2110f
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-10-14 00:51:11 +0900

    ✅ Fix test
    Some versions of postcss cannot get the correct version from Processor#version property.

--- test/postcss@latest/postcss-version.ts
+++ test/postcss@latest/postcss-version.ts
@@ -4,5 +4,36 @@ import postcss from 'postcss';
 
 test('should match postcss version', t => {
     const version = (/^postcss@(.+)$/.exec(path.basename(__dirname)) || [])[1];
-    t.is(postcss().version, version === 'latest' ? '7.0.18' : version);
+
+    if (
+        [
+            '5.0.15',
+            '5.0.16',
+            '5.0.17',
+            '5.0.18',
+            '5.0.19',
+            '5.0.20',
+            '5.0.21',
+        ].includes(version)
+    ) {
+        /**
+         * postcss after version 5.0.15 forgot to update the "version" property until it was updated to 5.1.0.
+         * @see https://github.com/postcss/postcss/blob/5.0.15/lib/processor.es6#L5
+         * @see https://github.com/postcss/postcss/blob/5.0.16/lib/processor.es6#L5
+         * @see https://github.com/postcss/postcss/blob/5.0.17/lib/processor.es6#L5
+         * @see https://github.com/postcss/postcss/blob/5.0.18/lib/processor.es6#L5
+         * @see https://github.com/postcss/postcss/blob/5.0.19/lib/processor.es6#L5
+         * @see https://github.com/postcss/postcss/blob/5.0.20/lib/processor.es6#L5
+         * @see https://github.com/postcss/postcss/blob/5.0.21/lib/processor.es6#L5
+         */
+        t.is(postcss().version, '5.0.14');
+    } else if (version === '5.2.1') {
+        /**
+         * postcss@5.2.1 forgot to update the version property.
+         * @see https://github.com/postcss/postcss/blob/5.2.1/lib/processor.es6#L110
+         */
+        t.is(postcss().version, '5.2.0');
+    } else {
+        t.is(postcss().version, version === 'latest' ? '7.0.18' : version);
+    }
 });
