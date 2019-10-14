32d78c50ac338eb001210bedc83aa2454e093c90
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-10-15 01:37:27 +0900

    ♻️ Reduce the number of lines of loadPlugin() function and loadPlugins() function

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,10 +1,17 @@
 import importCwd from 'import-cwd';
-import util from 'util';
 
 import { defaultOptions, InputOptionsInterface } from './options';
-import { isObject } from './utils';
+import { isObject, toJsPath } from './utils';
 import { AcceptedPlugin, isAcceptedPlugin } from './utils/postcss';
 import { isReadonlyOrWritableArray } from './utils/types';
+
+function importPlugin(pluginName: string): unknown {
+    try {
+        return importCwd(pluginName);
+    } catch (err) {
+        throw new Error(`Loading PostCSS Plugin failed: ${err.message}`);
+    }
+}
 
 /**
  * Plugin Loader
@@ -15,18 +22,12 @@ function loadPlugin(
     pluginOptions: unknown,
     propList: (string | number)[],
 ): AcceptedPlugin {
-    let pluginGenerator: unknown;
-    try {
-        pluginGenerator = importCwd(pluginName);
-    } catch (err) {
-        throw new Error(`Loading PostCSS Plugin failed: ${err.message}`);
-    }
-
-    let plugin = pluginGenerator;
+    const pluginGenerator = importPlugin(pluginName);
 
     /**
      * @see https://github.com/michael-ciniawsky/postcss-load-config/blob/v2.1.0/src/plugins.js#L18-L26
      */
+    let plugin = pluginGenerator;
     if (isObject(pluginOptions) && Object.keys(pluginOptions).length > 0) {
         if (typeof pluginGenerator !== 'function') {
             throw new TypeError(
@@ -39,32 +40,20 @@ function loadPlugin(
     /**
      * @see https://github.com/michael-ciniawsky/postcss-load-config/blob/v2.1.0/src/plugins.js#L59-L61
      */
-    if (isObject(plugin) && plugin.postcss) {
-        plugin = plugin.postcss;
-    }
+    if (isObject(plugin) && plugin.postcss) plugin = plugin.postcss;
 
     /**
      * @see https://github.com/michael-ciniawsky/postcss-load-config/blob/v2.1.0/src/plugins.js#L63-L65
      */
-    if (isObject(plugin) && plugin.default) {
-        plugin = plugin.default;
-    }
+    if (isObject(plugin) && plugin.default) plugin = plugin.default;
 
     /**
      * @see https://github.com/michael-ciniawsky/postcss-load-config/blob/v2.1.0/src/plugins.js#L67-L73
      */
-    if (!isAcceptedPlugin(plugin)) {
+    if (!isAcceptedPlugin(plugin))
         throw new TypeError(
-            'Invalid PostCSS Plugin found at: plugins' +
-                propList
-                    .map(prop =>
-                        typeof prop === 'string' && /^\w+$/.test(prop)
-                            ? `.${prop}`
-                            : `[${util.inspect(prop)}]`,
-                    )
-                    .join(''),
+            `Invalid PostCSS Plugin found at: plugins${toJsPath(propList)}`,
         );
-    }
 
     return plugin;
 }
@@ -76,42 +65,29 @@ export function loadPlugins(
     plugins: InputOptionsInterface['plugins'] | undefined,
     propList: (string | number)[] = [],
 ): ReadonlyArray<AcceptedPlugin> {
-    if (!plugins) {
-        return defaultOptions.plugins;
-    }
+    if (!plugins) return defaultOptions.plugins;
 
-    let pluginList: AcceptedPlugin[] = [];
-
-    if ((Array.isArray as isReadonlyOrWritableArray)(plugins)) {
-        pluginList = [...plugins]
-            .map((plugin, index) => {
-                if (isAcceptedPlugin(plugin)) {
-                    return plugin;
-                }
-
-                if (typeof plugin === 'string') {
-                    return [loadPlugin(plugin, null, [...propList, index])];
-                }
-
-                return loadPlugins(plugin, [...propList, index]);
-            })
-            .reduce<typeof pluginList>(
-                (list, plugins) => list.concat(plugins),
-                [],
-            );
-    } else {
-        pluginList = Object.entries(plugins)
-            /**
-             * @see https://github.com/michael-ciniawsky/postcss-load-config/blob/v2.1.0/src/plugins.js#L49-L51
-             */
-            .filter(([, pluginOptions]) => pluginOptions !== false)
-            .map(([pluginName, pluginOptions]) =>
-                loadPlugin(pluginName, pluginOptions, [
-                    ...propList,
-                    pluginName,
-                ]),
-            );
-    }
-
-    return pluginList;
+    return (Array.isArray as isReadonlyOrWritableArray)(plugins)
+        ? [...plugins]
+              .map((plugin, index) => {
+                  if (isAcceptedPlugin(plugin)) return plugin;
+                  if (typeof plugin === 'string')
+                      return [loadPlugin(plugin, null, [...propList, index])];
+                  return loadPlugins(plugin, [...propList, index]);
+              })
+              .reduce<AcceptedPlugin[]>(
+                  (list, plugins) => list.concat(plugins),
+                  [],
+              )
+        : Object.entries(plugins)
+              /**
+               * @see https://github.com/michael-ciniawsky/postcss-load-config/blob/v2.1.0/src/plugins.js#L49-L51
+               */
+              .filter(([, pluginOptions]) => pluginOptions !== false)
+              .map(([pluginName, pluginOptions]) =>
+                  loadPlugin(pluginName, pluginOptions, [
+                      ...propList,
+                      pluginName,
+                  ]),
+              );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import util from 'util';
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isObject(value: unknown): value is Record<any, unknown> {
     return typeof value === 'object' && value !== null;
@@ -8,4 +10,14 @@ export function hasProp<
     U extends (Parameters<typeof Object.prototype.hasOwnProperty>)[0]
 >(value: T, prop: U): value is T & Required<Pick<T, Extract<keyof T, U>>> {
     return Object.prototype.hasOwnProperty.call(value, prop);
+}
+
+export function toJsPath(propList: (string | number)[]): string {
+    return propList
+        .map(prop =>
+            typeof prop === 'string' && /^\w+$/.test(prop)
+                ? `.${prop}`
+                : `[${util.inspect(prop)}]`,
+        )
+        .join('');
 }
