e27892219e242a64986d6bb547b6022f06799c56
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-10-14 09:02:12 +0900

    🔥 Remove old test generation scripts and automatically generated files

--- test/helpers/localPlugins.ts
+++
@@ -1,8 +0,0 @@
-import pluginMap from '../../packages/metalsmith-postcss2/all-pkgs';
-
-export default Object.values(pluginMap).map(
-    ({ peerDeps: { postcss: postcssVersion }, module }) => ({
-        postcssLocalPlugin: module,
-        testNameSuffix: `/ postcss@${postcssVersion}`,
-    }),
-);
