83c0e748180c459eff4bcf07a00625010ead5078
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-10-15 02:27:48 +0900

    ♻️ Reduce Cognitive Complexity of updatePostcssOption() function

--- src/index.ts
+++ src/index.ts
@@ -65,10 +65,7 @@ function updatePostcssOption(
         );
         if (sourceMapFiledata) {
             const prev = sourceMapFiledata.contents.toString();
-            postcssOptions.map =
-                postcssMapOption === true
-                    ? { prev }
-                    : { prev, ...postcssMapOption };
+            postcssOptions.map = { prev, ...Object(postcssMapOption) };
         }
     }
 
