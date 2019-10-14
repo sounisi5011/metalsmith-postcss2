0b3488b921a856c3e72730693c7d974a21d4783c
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-10-14 09:26:21 +0900

    ⏪ Revert "✅ Run tests with multiple versions of postcss"
    This reverts commit 92afc8388e6917c7b695f42d9136a22fcd0c8aba.

--- test/import.ts
+++ test/import.ts
@@ -2,290 +2,285 @@ import test from 'ava';
 import Metalsmith from 'metalsmith';
 import path from 'path';
 
+import postcss from '../src/index';
 import { switchTest } from './helpers';
-import localPlugins from './helpers/localPlugins';
 import { debuggerPlugin, processAsync } from './helpers/metalsmith';
 
 const fixtures = path.join.bind(path, __dirname, 'fixtures');
 
-for (const { postcssLocalPlugin, testNameSuffix } of localPlugins) {
-    test(`should transform css files ${testNameSuffix}`, async t => {
-        const metalsmith = Metalsmith(fixtures('import'))
-            .source('src')
-            .use(
-                postcssLocalPlugin((_files, _metalsmith, defaultOptions) => ({
-                    pattern: ([] as string[]).concat(
-                        defaultOptions.pattern,
-                        '!**/_*',
-                        '!**/_*/**',
-                    ),
-                })),
-            );
-        const files = await processAsync(metalsmith);
+test('should transform css files', async t => {
+    const metalsmith = Metalsmith(fixtures('import'))
+        .source('src')
+        .use(
+            postcss((_files, _metalsmith, defaultOptions) => ({
+                pattern: ([] as string[]).concat(
+                    defaultOptions.pattern,
+                    '!**/_*',
+                    '!**/_*/**',
+                ),
+            })),
+        );
+    const files = await processAsync(metalsmith);
 
-        switchTest(
-            files['main.css'],
-            'should generate CSS file',
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files);
-            },
-        );
+    switchTest(
+        files['main.css'],
+        'should generate CSS file',
+        msg => {
+            t.pass(msg);
+        },
+        msg => {
+            t.fail(msg);
+            t.log(files);
+        },
+    );
 
-        t.regex(
-            files['main.css'].contents.toString(),
-            /^(?:\/\*(?:(?!\*\/)[\s\S])*\*\/|\s+)*\.bold\s*\{\s*font-weight:\s*bold\s*(?:;\s*)?\}\s*(?:\/\*(?:(?!\*\/)[\s\S])*\*\/|\s+)*body\s*\{\s*margin:\s*0\s*(?:;\s*)?\}\s*$/,
-        );
-    });
+    t.regex(
+        files['main.css'].contents.toString(),
+        /^(?:\/\*(?:(?!\*\/)[\s\S])*\*\/|\s+)*\.bold\s*\{\s*font-weight:\s*bold\s*(?:;\s*)?\}\s*(?:\/\*(?:(?!\*\/)[\s\S])*\*\/|\s+)*body\s*\{\s*margin:\s*0\s*(?:;\s*)?\}\s*$/,
+    );
+});
 
-    test(`should transform css files with deps ${testNameSuffix}`, async t => {
-        const dependenciesKey = 'deps';
-        let beforeFiles: Metalsmith.Files = {};
-        const metalsmith = Metalsmith(fixtures('import'))
-            .source('src')
-            .use(
-                debuggerPlugin(files => {
-                    beforeFiles = { ...files };
-                }),
-            )
-            .use(
-                postcssLocalPlugin((_files, _metalsmith, defaultOptions) => ({
-                    pattern: ([] as string[]).concat(
-                        defaultOptions.pattern,
-                        '!**/_*',
-                        '!**/_*/**',
-                    ),
-                    dependenciesKey,
-                })),
-            );
-        const files = await processAsync(metalsmith);
+test('should transform css files with deps', async t => {
+    const dependenciesKey = 'deps';
+    let beforeFiles: Metalsmith.Files = {};
+    const metalsmith = Metalsmith(fixtures('import'))
+        .source('src')
+        .use(
+            debuggerPlugin(files => {
+                beforeFiles = { ...files };
+            }),
+        )
+        .use(
+            postcss((_files, _metalsmith, defaultOptions) => ({
+                pattern: ([] as string[]).concat(
+                    defaultOptions.pattern,
+                    '!**/_*',
+                    '!**/_*/**',
+                ),
+                dependenciesKey,
+            })),
+        );
+    const files = await processAsync(metalsmith);
 
-        switchTest(
-            files['main.css'],
-            'should generate CSS file',
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files);
-            },
-        )(
-            files['main.css'][dependenciesKey],
-            `should have "${dependenciesKey}" property in CSS file data`,
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files['main.css']);
-            },
-        );
+    switchTest(
+        files['main.css'],
+        'should generate CSS file',
+        msg => {
+            t.pass(msg);
+        },
+        msg => {
+            t.fail(msg);
+            t.log(files);
+        },
+    )(
+        files['main.css'][dependenciesKey],
+        `should have "${dependenciesKey}" property in CSS file data`,
+        msg => {
+            t.pass(msg);
+        },
+        msg => {
+            t.fail(msg);
+            t.log(files['main.css']);
+        },
+    );
 
-        t.deepEqual(
-            Object.keys(files['main.css'][dependenciesKey]).sort(),
-            ['main.css', '_sub.css', '_mod.css', 'dir/_child.css'].sort(),
-        );
-        t.is(Object.keys(files['main.css'][dependenciesKey])[0], 'main.css');
+    t.deepEqual(
+        Object.keys(files['main.css'][dependenciesKey]).sort(),
+        ['main.css', '_sub.css', '_mod.css', 'dir/_child.css'].sort(),
+    );
+    t.is(Object.keys(files['main.css'][dependenciesKey])[0], 'main.css');
 
-        t.is(
-            files['main.css'][dependenciesKey]['main.css'],
-            beforeFiles['main.css'],
-        );
-        t.is(
-            files['main.css'][dependenciesKey]['_sub.css'],
-            beforeFiles['_sub.css'],
-        );
-        t.is(
-            files['main.css'][dependenciesKey]['_mod.css'],
-            beforeFiles['_mod.css'],
-        );
-        t.is(
-            files['main.css'][dependenciesKey]['dir/_child.css'],
-            beforeFiles['dir/_child.css'],
-        );
-    });
+    t.is(
+        files['main.css'][dependenciesKey]['main.css'],
+        beforeFiles['main.css'],
+    );
+    t.is(
+        files['main.css'][dependenciesKey]['_sub.css'],
+        beforeFiles['_sub.css'],
+    );
+    t.is(
+        files['main.css'][dependenciesKey]['_mod.css'],
+        beforeFiles['_mod.css'],
+    );
+    t.is(
+        files['main.css'][dependenciesKey]['dir/_child.css'],
+        beforeFiles['dir/_child.css'],
+    );
+});
 
-    test(`should transform css files with deps and SourceMap ${testNameSuffix}`, async t => {
-        const dependenciesKey = 'deps';
-        let beforeFiles: Metalsmith.Files = {};
-        const metalsmith = Metalsmith(fixtures('import'))
-            .source('src')
-            .use(
-                debuggerPlugin(files => {
-                    beforeFiles = { ...files };
-                }),
-            )
-            .use(
-                postcssLocalPlugin((_files, _metalsmith, defaultOptions) => ({
-                    pattern: ([] as string[]).concat(
-                        defaultOptions.pattern,
-                        '!**/_*',
-                        '!**/_*/**',
-                    ),
-                    options: {
-                        map: { inline: false },
-                    },
-                    dependenciesKey,
-                })),
-            );
-        const files = await processAsync(metalsmith);
+test('should transform css files with deps and SourceMap', async t => {
+    const dependenciesKey = 'deps';
+    let beforeFiles: Metalsmith.Files = {};
+    const metalsmith = Metalsmith(fixtures('import'))
+        .source('src')
+        .use(
+            debuggerPlugin(files => {
+                beforeFiles = { ...files };
+            }),
+        )
+        .use(
+            postcss((_files, _metalsmith, defaultOptions) => ({
+                pattern: ([] as string[]).concat(
+                    defaultOptions.pattern,
+                    '!**/_*',
+                    '!**/_*/**',
+                ),
+                options: {
+                    map: { inline: false },
+                },
+                dependenciesKey,
+            })),
+        );
+    const files = await processAsync(metalsmith);
 
-        switchTest(
-            files['main.css'],
-            'should generate CSS file',
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files);
-            },
-        )(
-            files['main.css'][dependenciesKey],
-            `should have "${dependenciesKey}" property in CSS file data`,
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files['main.css']);
-            },
-        )(
-            files['main.css.map'],
-            'should generate SourceMap file',
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files);
-            },
-        )(
-            files['main.css.map'][dependenciesKey],
-            `should have "${dependenciesKey}" property in SourceMap file data`,
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files['main.css.map']);
-            },
-        );
+    switchTest(
+        files['main.css'],
+        'should generate CSS file',
+        msg => {
+            t.pass(msg);
+        },
+        msg => {
+            t.fail(msg);
+            t.log(files);
+        },
+    )(
+        files['main.css'][dependenciesKey],
+        `should have "${dependenciesKey}" property in CSS file data`,
+        msg => {
+            t.pass(msg);
+        },
+        msg => {
+            t.fail(msg);
+            t.log(files['main.css']);
+        },
+    )(
+        files['main.css.map'],
+        'should generate SourceMap file',
+        msg => {
+            t.pass(msg);
+        },
+        msg => {
+            t.fail(msg);
+            t.log(files);
+        },
+    )(
+        files['main.css.map'][dependenciesKey],
+        `should have "${dependenciesKey}" property in SourceMap file data`,
+        msg => {
+            t.pass(msg);
+        },
+        msg => {
+            t.fail(msg);
+            t.log(files['main.css.map']);
+        },
+    );
 
-        t.deepEqual(
-            Object.keys(files['main.css'][dependenciesKey]).sort(),
-            ['main.css', '_sub.css', '_mod.css', 'dir/_child.css'].sort(),
-        );
-        t.is(Object.keys(files['main.css'][dependenciesKey])[0], 'main.css');
+    t.deepEqual(
+        Object.keys(files['main.css'][dependenciesKey]).sort(),
+        ['main.css', '_sub.css', '_mod.css', 'dir/_child.css'].sort(),
+    );
+    t.is(Object.keys(files['main.css'][dependenciesKey])[0], 'main.css');
 
-        t.is(
-            files['main.css'][dependenciesKey]['main.css'],
-            beforeFiles['main.css'],
-        );
-        t.is(
-            files['main.css'][dependenciesKey]['_sub.css'],
-            beforeFiles['_sub.css'],
-        );
-        t.is(
-            files['main.css'][dependenciesKey]['_mod.css'],
-            beforeFiles['_mod.css'],
-        );
-        t.is(
-            files['main.css'][dependenciesKey]['dir/_child.css'],
-            beforeFiles['dir/_child.css'],
-        );
+    t.is(
+        files['main.css'][dependenciesKey]['main.css'],
+        beforeFiles['main.css'],
+    );
+    t.is(
+        files['main.css'][dependenciesKey]['_sub.css'],
+        beforeFiles['_sub.css'],
+    );
+    t.is(
+        files['main.css'][dependenciesKey]['_mod.css'],
+        beforeFiles['_mod.css'],
+    );
+    t.is(
+        files['main.css'][dependenciesKey]['dir/_child.css'],
+        beforeFiles['dir/_child.css'],
+    );
 
-        t.deepEqual(
-            Object.keys(files['main.css.map'][dependenciesKey]).sort(),
-            ['main.css', '_sub.css', '_mod.css', 'dir/_child.css'].sort(),
-        );
-        t.is(
-            Object.keys(files['main.css.map'][dependenciesKey])[0],
-            'main.css',
-        );
+    t.deepEqual(
+        Object.keys(files['main.css.map'][dependenciesKey]).sort(),
+        ['main.css', '_sub.css', '_mod.css', 'dir/_child.css'].sort(),
+    );
+    t.is(Object.keys(files['main.css.map'][dependenciesKey])[0], 'main.css');
 
-        t.is(
-            files['main.css.map'][dependenciesKey]['main.css'],
-            beforeFiles['main.css'],
-        );
-        t.is(
-            files['main.css.map'][dependenciesKey]['_sub.css'],
-            beforeFiles['_sub.css'],
-        );
-        t.is(
-            files['main.css.map'][dependenciesKey]['_mod.css'],
-            beforeFiles['_mod.css'],
-        );
-        t.is(
-            files['main.css.map'][dependenciesKey]['dir/_child.css'],
-            beforeFiles['dir/_child.css'],
-        );
-    });
+    t.is(
+        files['main.css.map'][dependenciesKey]['main.css'],
+        beforeFiles['main.css'],
+    );
+    t.is(
+        files['main.css.map'][dependenciesKey]['_sub.css'],
+        beforeFiles['_sub.css'],
+    );
+    t.is(
+        files['main.css.map'][dependenciesKey]['_mod.css'],
+        beforeFiles['_mod.css'],
+    );
+    t.is(
+        files['main.css.map'][dependenciesKey]['dir/_child.css'],
+        beforeFiles['dir/_child.css'],
+    );
+});
 
-    test(`should transform css files with deps and forget ignore pattern ${testNameSuffix}`, async t => {
-        const dependenciesKey = 'deps';
-        let beforeFiles: Metalsmith.Files = {};
-        const metalsmith = Metalsmith(fixtures('import'))
-            .source('src')
-            .use(
-                debuggerPlugin(files => {
-                    beforeFiles = { ...files };
-                }),
-            )
-            .use(
-                postcssLocalPlugin({
-                    dependenciesKey,
-                }),
-            );
-        const files = await processAsync(metalsmith);
+test('should transform css files with deps and forget ignore pattern', async t => {
+    const dependenciesKey = 'deps';
+    let beforeFiles: Metalsmith.Files = {};
+    const metalsmith = Metalsmith(fixtures('import'))
+        .source('src')
+        .use(
+            debuggerPlugin(files => {
+                beforeFiles = { ...files };
+            }),
+        )
+        .use(
+            postcss({
+                dependenciesKey,
+            }),
+        );
+    const files = await processAsync(metalsmith);
 
-        switchTest(
-            files['main.css'],
-            'should generate CSS file',
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files);
-            },
-        )(
-            files['main.css'][dependenciesKey],
-            `should have "${dependenciesKey}" property in CSS file data`,
-            msg => {
-                t.pass(msg);
-            },
-            msg => {
-                t.fail(msg);
-                t.log(files['main.css']);
-            },
-        );
+    switchTest(
+        files['main.css'],
+        'should generate CSS file',
+        msg => {
+            t.pass(msg);
+        },
+        msg => {
+            t.fail(msg);
+            t.log(files);
+        },
+    )(
+        files['main.css'][dependenciesKey],
+        `should have "${dependenciesKey}" property in CSS file data`,
+        msg => {
+            t.pass(msg);
+        },
+        msg => {
+            t.fail(msg);
+            t.log(files['main.css']);
+        },
+    );
 
-        t.deepEqual(
-            Object.keys(files['main.css'][dependenciesKey]).sort(),
-            ['main.css', '_sub.css', '_mod.css', 'dir/_child.css'].sort(),
-        );
-        t.is(Object.keys(files['main.css'][dependenciesKey])[0], 'main.css');
+    t.deepEqual(
+        Object.keys(files['main.css'][dependenciesKey]).sort(),
+        ['main.css', '_sub.css', '_mod.css', 'dir/_child.css'].sort(),
+    );
+    t.is(Object.keys(files['main.css'][dependenciesKey])[0], 'main.css');
 
-        t.is(
-            files['main.css'][dependenciesKey]['main.css'],
-            beforeFiles['main.css'],
-        );
-        t.is(
-            files['main.css'][dependenciesKey]['_sub.css'],
-            beforeFiles['_sub.css'],
-        );
-        t.is(
-            files['main.css'][dependenciesKey]['_mod.css'],
-            beforeFiles['_mod.css'],
-        );
-        t.is(
-            files['main.css'][dependenciesKey]['dir/_child.css'],
-            beforeFiles['dir/_child.css'],
-        );
-    });
-}
+    t.is(
+        files['main.css'][dependenciesKey]['main.css'],
+        beforeFiles['main.css'],
+    );
+    t.is(
+        files['main.css'][dependenciesKey]['_sub.css'],
+        beforeFiles['_sub.css'],
+    );
+    t.is(
+        files['main.css'][dependenciesKey]['_mod.css'],
+        beforeFiles['_mod.css'],
+    );
+    t.is(
+        files['main.css'][dependenciesKey]['dir/_child.css'],
+        beforeFiles['dir/_child.css'],
+    );
+});
--- test/prohibit-options.ts
+++ test/prohibit-options.ts
@@ -2,84 +2,82 @@ import test from 'ava';
 import Metalsmith from 'metalsmith';
 import path from 'path';
 
-import localPlugins from './helpers/localPlugins';
+import postcss from '../src/index';
 import { processAsync } from './helpers/metalsmith';
 
 const fixtures = path.join.bind(path, __dirname, 'fixtures');
 
-for (const { postcssLocalPlugin, testNameSuffix } of localPlugins) {
-    test(`should throw error if set "from" option: plugin options ${testNameSuffix}`, async t => {
-        const metalsmith = Metalsmith(fixtures('sugarss'))
-            .source('src')
-            .use(
-                postcssLocalPlugin({
-                    options: { from: 'path/to/src.css' } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
-                }),
-            );
+test('should throw error if set "from" option: plugin options', async t => {
+    const metalsmith = Metalsmith(fixtures('sugarss'))
+        .source('src')
+        .use(
+            postcss({
+                options: { from: 'path/to/src.css' } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+            }),
+        );
 
-        await t.throwsAsync(processAsync(metalsmith), {
-            message: /Can not set "from" option in plugin options/,
-        });
+    await t.throwsAsync(processAsync(metalsmith), {
+        message: /Can not set "from" option in plugin options/,
     });
+});
 
-    test(`should throw error if set "from" option: config file ${testNameSuffix}`, async t => {
-        const metalsmith = Metalsmith(fixtures('options-from'))
-            .source('src')
-            .use(postcssLocalPlugin());
+test('should throw error if set "from" option: config file', async t => {
+    const metalsmith = Metalsmith(fixtures('options-from'))
+        .source('src')
+        .use(postcss());
 
-        await t.throwsAsync(processAsync(metalsmith), {
-            message: /Can not set "from" option in config file/,
-        });
+    await t.throwsAsync(processAsync(metalsmith), {
+        message: /Can not set "from" option in config file/,
     });
+});
 
-    test(`should throw error if set "to" option: plugin options ${testNameSuffix}`, async t => {
-        const metalsmith = Metalsmith(fixtures('sugarss'))
-            .source('src')
-            .use(
-                postcssLocalPlugin({
-                    options: { to: 'path/to/dest.css' } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
-                }),
-            );
+test('should throw error if set "to" option: plugin options', async t => {
+    const metalsmith = Metalsmith(fixtures('sugarss'))
+        .source('src')
+        .use(
+            postcss({
+                options: { to: 'path/to/dest.css' } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+            }),
+        );
 
-        await t.throwsAsync(processAsync(metalsmith), {
-            message: /Can not set "to" option in plugin options/,
-        });
+    await t.throwsAsync(processAsync(metalsmith), {
+        message: /Can not set "to" option in plugin options/,
     });
+});
 
-    test(`should throw error if set "to" option: config file ${testNameSuffix}`, async t => {
-        const metalsmith = Metalsmith(fixtures('options-to'))
-            .source('src')
-            .use(postcssLocalPlugin());
+test('should throw error if set "to" option: config file', async t => {
+    const metalsmith = Metalsmith(fixtures('options-to'))
+        .source('src')
+        .use(postcss());
 
-        await t.throwsAsync(processAsync(metalsmith), {
-            message: /Can not set "to" option in config file/,
-        });
+    await t.throwsAsync(processAsync(metalsmith), {
+        message: /Can not set "to" option in config file/,
     });
+});
 
-    test(`should throw error if set "from" and "to" options: plugin options ${testNameSuffix}`, async t => {
-        const metalsmith = Metalsmith(fixtures('sugarss'))
-            .source('src')
-            .use(
-                postcssLocalPlugin({
-                    options: {
-                        from: 'path/to/src.css',
-                        to: 'path/to/dest.css',
-                    } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
-                }),
-            );
+test('should throw error if set "from" and "to" options: plugin options', async t => {
+    const metalsmith = Metalsmith(fixtures('sugarss'))
+        .source('src')
+        .use(
+            postcss({
+                options: {
+                    from: 'path/to/src.css',
+                    to: 'path/to/dest.css',
+                } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+            }),
+        );
 
-        await t.throwsAsync(processAsync(metalsmith), {
-            message: /Can not set "from" and "to" options in plugin options/,
-        });
+    await t.throwsAsync(processAsync(metalsmith), {
+        message: /Can not set "from" and "to" options in plugin options/,
     });
+});
 
-    test(`should throw error if set "from" and "to" options: config file ${testNameSuffix}`, async t => {
-        const metalsmith = Metalsmith(fixtures('options-from-and-to'))
-            .source('src')
-            .use(postcssLocalPlugin());
+test('should throw error if set "from" and "to" options: config file', async t => {
+    const metalsmith = Metalsmith(fixtures('options-from-and-to'))
+        .source('src')
+        .use(postcss());
 
-        await t.throwsAsync(processAsync(metalsmith), {
-            message: /Can not set "from" and "to" options in config file/,
-        });
+    await t.throwsAsync(processAsync(metalsmith), {
+        message: /Can not set "from" and "to" options in config file/,
     });
-}
+});
